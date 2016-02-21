c7a0cb40872e7be824362219aa44b9f6addf6e9c
Bojan Vujatovic <bojan.vujatovic@gmail.com>
2016-02-21 05:05:38 +0100

    Fully functioning first example

--- pca_example_1.py
+++ pca_example_1.py
@@ -2,6 +2,7 @@ from sklearn.datasets import fetch_mldata
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 import numpy as np
 
 digits = fetch_mldata('MNIST original')
@@ -15,7 +16,8 @@ X_train, y_train = digits.data[:N_train, :], digits.target[:N_train]
 X_test, y_test = digits.data[N_train:, :], digits.target[N_train:]
 
 knn = KNeighborsClassifier(n_neighbors=5)
-n_pcs = np.linspace(1, d, 50).astype(int)
+n_pcs = np.linspace(1, d, 10).astype(int)
+accuracies = []
 
 for n_pc in n_pcs:
 	pca = PCA(n_components=n_pc)
@@ -26,5 +28,7 @@ for n_pc in n_pcs:
 	knn.fit(X_train_reduced, y_train)
 	y_pred = knn.predict(X_test_reduced)
 	accuracy = accuracy_score(y_test, y_pred)
+	accuracies.append(accuracy)
 
-	print(n_pc, accuracy)
+plt.plot(n_pcs, accuracies)
+plt.show()
